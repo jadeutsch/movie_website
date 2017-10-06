5017615e0e54aa87d02e35d59df9753c33a027e7
Jeff <deutsch.jeff.a@gmail.com>
2017-10-06 16:56:31 -0500

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,11 @@
+import media
+
+# sw7 is Star Was: The Force Awakens
+sw7 = media.Movie(
+    "Star Wars: The Force Awakens",
+    "A scavenger and a renegade stormtrooper enlist the help of legendary smugglers/freedom fighters Han Solo and Chewbacca to transport a droid carrying information regarding the whereabouts of long lost Jedi Master Luke Skywalker to General Leia Organa of the Resistance before it falls into the hands of Kylo Ren and the First Order",
+    "https://en.wikipedia.org/wiki/Star_Wars:_The_Force_Awakens#/media/File:Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg",
+    "https://youtu.be/sGbxmsDFVnE"
+)
+
+print sw7.summary
---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(self, movie_title, movie_summary, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.summary = movie_summary
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
